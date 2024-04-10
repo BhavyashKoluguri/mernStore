7114a24a3a6f01a6b0d42504736c984f50346e5e
BhavyashKoluguri <102355627+BhavyashKoluguri@users.noreply.github.com>
2024-04-10 22:29:14 +0530

    added changes in index

--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Contact from './pages/Contact';
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode> 
+    
+    
+      <BrowserRouter>
+      <App />
+      <Routes>
+            
+            <Route path='/contact' element={<Contact />}></Route>
+            
+        </Routes>
+      </BrowserRouter>
+    
   </React.StrictMode>
 );
 
